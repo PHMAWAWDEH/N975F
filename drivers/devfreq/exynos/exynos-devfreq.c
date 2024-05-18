7edfbf940ca7ff871d2c3930ec0291d673beba90
Hussein995 <36462714+Hussein995@users.noreply.github.com>
2024-05-18 02:25:45 -0700

    Update exynos-devfreq.c

--- drivers/devfreq/exynos/exynos-devfreq.c
+++ drivers/devfreq/exynos/exynos-devfreq.c
@@ -1684,7 +1684,10 @@ static int exynos_devfreq_probe(struct platform_device *pdev)
 		ret = -EINVAL;
 		goto err_devfreq;
 	}
-
+	if (data->devfreq_type == DEVFREQ_MIF)
+		
+		devfreq_register_boost_device(DEVFREQ_EXYNOS_MIF, data->devfreq);
+	
 	data->devfreq->min_freq = data->min_freq;
 	data->devfreq->max_freq = data->max_freq;
 
