c36e445638b4792720dcf4ba5b75a1639cbd1db2
Hussein995 <36462714+Hussein995@users.noreply.github.com>
2024-05-21 14:08:14 -0700

    nnn

--- drivers/vision/npu/generated/npu-ver-info.c
+++ drivers/vision/npu/generated/npu-ver-info.c
@@ -1,3 +1,3 @@
 const char *npu_git_log_str = "0000000";
 const char *npu_git_hash_str = "0000000";
-const char *npu_build_info = "ivanmeler@haxynox / Build on 2022-01-01 12:12:12+02:00";
+const char *npu_build_info = "ivanmeler@haxynox / Build on 2020-03-03 12:12:12+02:00";
